17eb92dc0d337e2bad0eec75b9521d4c2b65acf9
MSAugdal <mathias.s.augdal@gmail.com>
2023-07-06 10:18:02 +0200

    refactored: index.html, style.css, scripts.js scripts.js: brainstorming and pseudocode

--- scripts.js
+++ scripts.js
@@ -1,86 +1,81 @@
-let num1 = "";
-let num2 = "";
-let operator = "";
-let showDummy = true;
-let clickedOperator = false;
-let hasCalculated = false;
+let operand1,
+	operand2,
+	operator1,
+	operator2,
+	result = null;
+let displayValue = "0";
 
-let operators = {
-	"+": (x, y) => Number(x) + Number(y),
-	"-": (x, y) => Number(x) - Number(y),
-	"*": (x, y) => Number(x) * Number(y),
-	"/": (x, y) => Number(x) / Number(y),
-};
+const operands = document.querySelectorAll(".operand");
+const operators = document.querySelectorAll(".operator");
+const clear = document.querySelector(".clear");
+const sign = document.querySelector(".sign");
+const percent = document.querySelector(".percent");
+const equals = document.querySelector(".equals");
 
-function operate(operator, x, y) {
-	hasCalculated = true;
-	returnValue = operators[operator](x, y);
-	// num1 = "";
-	num2 = "";
-	operator = "";
-	clickedOperator = false;
-	return returnValue;
+operands.forEach((operand) => {
+	operand.addEventListener("click", (e) => {
+		operandClicked(e.target.value);
+	});
+});
+operators.forEach((operator) => {
+	operator.addEventListener("click", (e) => {
+		operatorClicked(e.target.value);
+	});
+});
+clear.addEventListener("click", (e) => {
+	operand1 = null;
+	operand2 = null;
+	operator1 = null;
+	operator2 = null;
+	result = null;
+	displayValue = "0";
+});
+
+function operate(operator, num1, num2) {
+	let operators = {
+		"+": (x, y) => Number(x) + Number(y),
+		"-": (x, y) => Number(x) - Number(y),
+		"*": (x, y) => Number(x) * Number(y),
+		"/": (x, y) => Number(x) / Number(y),
+	};
+	return operators[operator](num1, num2);
 }
-
-const display = document.getElementById("display-content");
 
 function updateScreen(value) {
-	display.innerText += value;
+	displayValue = value;
 }
 
-const buttons = document.querySelectorAll(".num");
-buttons.forEach((button) => {
-	button.addEventListener("click", (e) => {
-		if (showDummy === true) {
-			showDummy = false;
-			display.innerText = "";
-		}
-		if (hasCalculated && e.target.id !== "=") {
-			hasCalculated = false;
-			// display.innerText = "";
-		}
-	});
-});
+function operandClicked(operand) {
+	console.log(operand);
+	return;
+}
 
-const clear = document.getElementById("clear");
-clear.addEventListener("click", () => {
-	num1 = "";
-	num2 = "";
-	operator = "";
-	clickedOperator = false;
-	hasCalculated = false;
-	display.innerText = "";
-	console.log(num1, num2, operator);
-});
+function operatorClicked(operator) {
+	console.log(operator);
+	return;
+}
 
-const operatorButtons = document.querySelectorAll(".operator");
-operatorButtons.forEach((button) => {
-	button.addEventListener("click", (e) => {
-		if (e.target.id !== "=" && num1 !== "")
-			if (clickedOperator === false) {
-				clickedOperator = true;
-				operator = button.id;
-				updateScreen(operator);
-			}
-		if (clickedOperator === true && num2 !== "" && e.target.id == "=") {
-			display.innerText = operate(operator, num1, num2);
-			num1 = display.innerText;
-		}
-		console.log(num1, num2, operator);
-	});
-});
+// store all values (operand1-2, operator, displayValue)
+// event handler to get operand1-2 and operator1.
 
-const nums_container = document.querySelector(".nums");
-const nums = nums_container.querySelectorAll(".num");
-nums.forEach((num) => {
-	num.addEventListener("click", (e) => {
-		if (clickedOperator === false) {
-			num1 += e.target.id;
-			updateScreen(e.target.id);
-			return;
-		}
-		num2 += e.target.id;
-		updateScreen(e.target.id);
-		console.log(num1, num2, operator);
-	});
-});
+// operand clicked (operand):
+//      if operand1 === null
+//          displayValue = operand
+//
+//      else operand1 !== null && operator1 !== null:
+//
+//
+// operator clicked (operator):
+//      if operator1 === null and operator2 === null:
+//          operator1 = operator
+//          operand1 = displayValue
+//
+//
+//      if operator2 === operator1 and operands are populated, calculate again
+//      if operator 2 !== operator 1,
+// if new operator clicked is same as last, calculate again
+// function to get keystrokes and click buttons accordingly
+// handle when displayValue is longer than display can show?
+//
+// function to calculate and return result
+// update display with reuslt
